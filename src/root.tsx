4fe20eca547cfb3455acdff3b52458fbf96a4b4d
Vladislav Lukyanuk <vlukyanuk.job@gmail.com>
2020-10-24 12:02:00 +0600

    use types

--- src/root.tsx
+++ src/root.tsx
@@ -3,8 +3,9 @@ import * as React from 'react';
 import ReactPng from './assets/images/react.png';
 import { StyledDiv } from './root.styles';
 
-export const Root = () => (
+export const Root = ({ name = "text" }: { name?: string }) => (
     <div>
+        <div>{name}</div>
         <img src={ReactPng}/>
         <StyledDiv />
     </div>
